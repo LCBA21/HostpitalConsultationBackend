85b835fa91e460261d72fba006db6a9b9c15826f
Lesibana Ngoepe <lesiban56@gmail.com>
2026-01-24 13:39:24 +0200

    fix Case Spring Data JPA: Case Sensitivity in AppointmentRepository

---
+++ src/main/java/com/example/hospitalConsaltationDemo/appointment/repository/AppointmentRepository.java
@@ -0,0 +1,30 @@
+package com.example.hospitalConsaltationDemo.appointment.repository;
+
+import com.example.hospitalConsaltationDemo.appointment.entity.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+public interface AppointmentRepository extends JpaRepository<Appointment,Long> {
+
+    List<Appointment> findByDoctor_User_IdOrderByIdDesc(Long userId);
+
+    List<Appointment> findByPatient_User_IdOrderByIdDesc(Long userId);
+
+    @Query("SELECT a FROM Appointment a "+
+           "WHERE a.doctor.id = :doctorId "+
+            "AND a.status = 'SCHEDULED' " +
+            "AND (" +
+            " (a.startTime < :newEndTime AND a.endTime > :newStartTime)" +
+            ")")
+        List<Appointment> findConflictingAppointments(
+            @Param("doctorId") Long doctorId,
+            @Param("newStartTime")LocalDateTime newStartTime,
+            @Param("newEndTime")LocalDateTime newEndTime
+            );
+
+}
