151af0c7d37c8fd0b3de319bc8de2d734aff7e23
Lesibana Ngoepe <lesibana@gmail.com>
2026-01-24 03:54:00 +0200

    Add RoleRepository interface for role management functionality

---
+++ src/main/java/com/example/hospitalConsaltationDemo/role/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.example.hospitalConsaltationDemo.role.repository;
+
+import com.example.hospitalConsaltationDemo.role.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository  extends JpaRepository<Role,Long> {
+
+    Optional<Role> findByName(String name);
+
+}
