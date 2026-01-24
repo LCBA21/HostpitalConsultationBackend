f6d650bf97d8e17c762ed8415d4006f16562a50b
Lesibana Ngoepe <LCBA21>
2026-01-24 02:52:26 +0200

    Add initial project structure

---
+++ src/main/java/com/example/hospitalConsaltationDemo/HospitalConsaltationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.hospitalConsaltationDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalConsaltationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalConsaltationDemoApplication.class, args);
+	}
+
+}
