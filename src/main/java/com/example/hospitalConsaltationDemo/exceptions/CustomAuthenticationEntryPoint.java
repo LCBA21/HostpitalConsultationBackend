de5cd90481e4b196ad80cecdb010d563352f6b49
Lesibana Ngoepe <lesiban56@gmail.com>
2026-01-25 14:01:13 +0200

    fixed lombok class error

---
+++ src/main/java/com/example/hospitalConsaltationDemo/exceptions/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,38 @@
+package com.example.hospitalConsaltationDemo.exceptions;
+
+
+import com.example.hospitalConsaltationDemo.response.Response;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+import java.io.IOException;
+@Component
+@RequiredArgsConstructor
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException)
+            throws IOException {
+
+        Response<?> errorResponse = Response.builder()
+                .statusCode(HttpStatus.UNAUTHORIZED.value()) // 401
+                .message("Unauthorized: Invalid or missing authentication token")
+                .build();
+
+        response.setContentType("application/json");
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+    }
+}
