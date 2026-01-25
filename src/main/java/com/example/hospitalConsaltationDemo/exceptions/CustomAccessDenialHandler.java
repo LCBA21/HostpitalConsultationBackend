03aaed8e55c91a468a29f62d39736598d92d8787
Lesibana Ngoepe <lesiban56@gmail.com>
2026-01-25 13:58:49 +0200

    fixed lombok class error

---
+++ src/main/java/com/example/hospitalConsaltationDemo/exceptions/CustomAccessDenialHandler.java
@@ -0,0 +1,35 @@
+package com.example.hospitalConsaltationDemo.exceptions;
+
+import com.example.hospitalConsaltationDemo.response.Response;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class CustomAccessDenialHandler implements AccessDeniedHandler {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void handle(HttpServletRequest request,
+                       HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException {
+
+        Response<?> errorResponse = Response.builder()
+                .statusCode(HttpStatus.FORBIDDEN.value())
+                .message("Access Denied: You don't have permission to access this resource")
+                .build();
+
+        response.setContentType("application/json");
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+    }
+}
