f8cda53469d5c8e550f223bc1bdb4e840f5fa1cb
Lesibana Ngoepe <lesiban56@gmail.com>
2026-02-05 18:08:10 +0200

    add NotificationService and NotificationServiceImpl interfaces for notification handling

--- src/main/java/com/example/hospitalConsaltationDemo/notifications/service/NotificationService.java
+++ src/main/java/com/example/hospitalConsaltationDemo/notifications/service/NotificationService.java
@@ -1,2 +1,8 @@
-package com.example.hospitalConsaltationDemo.notifications.service;public interface NotificationService {
+package com.example.hospitalConsaltationDemo.notifications.service;
+
+import com.example.hospitalConsaltationDemo.notifications.dto.NotificationDTO;
+import com.example.hospitalConsaltationDemo.users.enity.User;
+
+public interface NotificationService {
+    void sendEmail(NotificationDTO notificationDTO, User user);
 }
--- src/main/java/com/example/hospitalConsaltationDemo/notifications/service/NotificationServiceImpl.java
+++ src/main/java/com/example/hospitalConsaltationDemo/notifications/service/NotificationServiceImpl.java
@@ -1,2 +1,75 @@
-package com.example.hospitalConsaltationDemo.notifications.service;public class NotificationServiceImpl {
+package com.example.hospitalConsaltationDemo.notifications.service;
+
+import com.example.hospitalConsaltationDemo.enums.NotificationType;
+import com.example.hospitalConsaltationDemo.notifications.dto.NotificationDTO;
+import com.example.hospitalConsaltationDemo.notifications.entity.Notification;
+import com.example.hospitalConsaltationDemo.notifications.repository.NotificationRepository;
+import com.example.hospitalConsaltationDemo.users.enity.User;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.nio.charset.StandardCharsets;
+
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class NotificationServiceImpl implements NotificationService {
+
+    private final NotificationRepository notificationRepository;
+    private final JavaMailSender mailSender;
+    private final TemplateEngine templateEngine;
+
+    @Override
+    @Async
+    public void sendEmail(NotificationDTO notificationDTO, User user) {
+
+        try {
+
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(
+                    mimeMessage,
+                    MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+                    StandardCharsets.UTF_8.name()
+            );
+
+            helper.setTo(notificationDTO.getRecipient());
+            helper.setSubject(notificationDTO.getSubject());
+
+            if (notificationDTO.getTemplateName() != null){
+
+                Context context = new Context();
+                context.setVariables(notificationDTO.getTemplateVariables());
+                String htmContent = templateEngine.process(notificationDTO.getTemplateName(),context);
+                helper.setText(htmContent,true);
+            }else {
+
+                helper.setText(notificationDTO.getMessage(),true);
+            }
+
+            mailSender.send(mimeMessage);
+            log.info("Email sent out");
+
+            Notification notificationToSave = Notification.builder()
+                    .recipient(notificationDTO.getRecipient())
+                    .subject(notificationDTO.getSubject())
+                    .message(notificationDTO.getMessage())
+                    .type(NotificationType.EMAIL)
+                    .user(user)
+                    .build();
+
+            notificationRepository.save(notificationToSave);
+
+        } catch (Exception e) {
+            log.info(e.getMessage());
+
+        }
+    }
 }
